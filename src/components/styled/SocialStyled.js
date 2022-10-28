8ae6363451b7122ca4b276a73a7807eadd3778e4
Sosaristic <sundayomena9@gmail.com>
2022-10-28 07:22:53 +0100

    modified: add margin top to  socialStyled

--- src/components/styled/SocialStyled.js
+++ src/components/styled/SocialStyled.js
@@ -4,9 +4,9 @@ export const SocialStyled = styled.div`
 position: relative;
 display: flex;
 justify-content: center;
-margin: 1rem 0;
+margin: 1.5rem 0;
 width: 100%;
-padding: .5rem 0;
+padding: 1rem 0;
 column-gap: 1.5rem;
 div{
     width: 20px;
