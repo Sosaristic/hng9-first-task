d138fd12e9216010838cbf818529838f7d8cf48f
Sosaristic <sundayomena9@gmail.com>
2022-10-28 09:44:40 +0100

    add github logo to social section

--- src/components/SocialSection.jsx
+++ src/components/SocialSection.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import { Container } from './styled/Container'
 import slackLogo from "./images/slack.png"
-import logo2 from "./images/I4G.png"
+import githublogo from "./images/github-logo.png"
 import { SocialStyled } from './styled/SocialStyled'
 
 export default function SocialSection() {
   return (
 <SocialStyled>      
       <div><img src={slackLogo} alt="" /></div>
-      <div><img src={slackLogo} alt="" /></div>
+      <div><img src={githublogo} alt="" /></div>
       
       </SocialStyled>
   )
